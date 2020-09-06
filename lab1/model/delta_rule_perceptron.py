0a1d588fbc58280e734c72815cc8f98a0b18a098
Frano Rajič <frano.rajich@gmail.com>
2020-09-06 18:34:52 +0200

    Added confmat to delta learning perceptron

--- lab1/model/delta_rule_perceptron.py
+++ lab1/model/delta_rule_perceptron.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 
 from utils.util import mse, extend_inputs_with_bias, shuffle_two_arrays, plot_metric, accuracy
@@ -6,6 +8,8 @@ from utils.util import mse, extend_inputs_with_bias, shuffle_two_arrays, plot_me
 class DeltaRulePerceptron:
     def __init__(self, inputs, targets, debug=False, save_folder=None, bias=True):
         self.W = np.random.randn(targets.shape[0], inputs.shape[0] + 1) / 2
+        if not bias:
+            self.W[:, -1] = np.zeros_like(self.W[:, -1])
         self.debug = debug
         self.save_folder = save_folder
         self.bias = bias
@@ -31,7 +35,7 @@ class DeltaRulePerceptron:
         return correct / targets.shape[1], loss
 
     def train(self, inputs, targets, eta, max_iter, batch_size, shuffle=False, stop_after=100):
-        inputs = extend_inputs_with_bias(inputs, features_axis=0, value=(1 if self.bias else 0))
+        inputs = extend_inputs_with_bias(inputs, features_axis=0)
 
         losses = []
         accuracies = []
@@ -47,7 +51,7 @@ class DeltaRulePerceptron:
             losses.append(loss)
 
             if acc > accuracies[pocket_epoch] or acc == accuracies[pocket_epoch] and loss + 1e-6 < losses[pocket_epoch]:
-                pocket_epoch = epoch
+                pocket_epoch = epoch  # TODO 1e-6 might be too big...
             else:
                 if (epoch - pocket_epoch) > stop_after:
                     break
@@ -65,7 +69,40 @@ class DeltaRulePerceptron:
         return weights_per_epoch, accuracies[pocket_epoch], losses[pocket_epoch], pocket_epoch
 
     def eval(self, inputs, targets):
-        self.forward(extend_inputs_with_bias(inputs, features_axis=0, value=(1 if self.bias else 0)))
+        self.forward(extend_inputs_with_bias(inputs, features_axis=0))
         acc = accuracy(targets, self.outputs, False)
         loss = mse(self.outputs_before_threshold, targets)
         return acc, loss
+
+    def conf_mat(self, inputs, targets):
+        self.forward(extend_inputs_with_bias(inputs, features_axis=0))
+        loss = mse(self.outputs_before_threshold, targets)
+
+        total = targets.shape[1]
+        correct = 0
+
+        nclasses = self.W.shape[0]
+        if nclasses == 1:
+            nclasses = 2
+            outputs = np.where(self.outputs > 0, 1, 0)
+            targets = np.where(targets > 0, 1, 0)
+        else:
+            # TODO has not been tested with one hoot
+            outputs = np.argmax(self.outputs_before_threshold, axis=0)
+            targets = np.argmax(targets, axis=0)
+
+        cm = np.zeros((nclasses, nclasses), dtype=int)
+        for i in range(nclasses):
+            for j in range(nclasses):
+                cm[i, j] = np.sum(np.where((outputs == i) & (targets == j), 1, 0))
+                if i == j:
+                    correct += cm[i, j]
+
+        acc = correct / total
+        if self.debug:
+            np.set_printoptions(threshold=sys.maxsize)
+            print(cm)
+            print(f"total:{total} correct:{correct}")
+            print(f"acc:{acc:2f}")
+
+        return cm
