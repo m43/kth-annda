e2d1a92b4c6cfe78102738f428154a72218250e4
Frano Rajič <frano.rajich@gmail.com>
2020-09-13 21:19:51 +0200

    RBF structure init

---
+++ model/rbf.py
@@ -0,0 +1,16 @@
+class RBF:
+    """
+    The Radial Basis Function network
+    """
+
+    def __init__(self, inputs, targets, n_rbf, sigma=0, usekmeans=False, normalise=False):
+        pass
+
+    def forward(self, inputs):
+        pass
+
+    def train(self, inputs, targets, eta, epochs):
+        pass
+
+    def confmat(self, inputs, targets):
+        pass
